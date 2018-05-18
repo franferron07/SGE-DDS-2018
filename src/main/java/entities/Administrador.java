a1e8d321b18a81b12a34f65ed05062272ff49af4
Luciana-PC\Luciana <equirozalayo@est.frba.utn.edu.ar>
2018-05-17 22:44:09 -0300

    Cantidad de Meses que lleva como administrador del sistema un Administrador

--- src/main/java/entities/Administrador.java
+++ src/main/java/entities/Administrador.java
@@ -1,36 +1,140 @@
 package entities;
 
-import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Administrador extends Usuario {
 
-	private LocalTime fechaAltaSistema;
+	private Date fechaAltaSistema;
 	private int identificadorSistema;
 
 	public Administrador() {
 
 	}
 	
-	//metodo que deberia hacer el calculo 
-	public void periodoDeAlta(){
-		
+	public long cantidadMesesComoAdmin(){
+		return getDiffDates(fechaAltaSistema,new Date(),1);
 	}
 
-	public LocalTime getFechaAltaSistema() {
-		return fechaAltaSistema;
-	}
 
 	public int getIdentificadorSistema() {
 		return identificadorSistema;
 	}
 
-	public void setFechaAltaSistema(LocalTime fechaAltaSistema) {
-		this.fechaAltaSistema = fechaAltaSistema;
-	}
-
 	public void setIdentificadorSistema(int identificadorSistema) {
 		this.identificadorSistema = identificadorSistema;
 	}
+	
+	private synchronized long getDiffDates(Date fechaInicio, Date fechaFin, int tipo) {
+		// Fecha inicio
+		Calendar calendarInicio = Calendar.getInstance();
+		calendarInicio.setTime(fechaInicio);
+		int diaInicio = calendarInicio.get(Calendar.DAY_OF_MONTH);
+		int mesInicio = calendarInicio.get(Calendar.MONTH) + 1; // 0 Enero, 11 Diciembre
+		int anioInicio = calendarInicio.get(Calendar.YEAR);	 
+		// Fecha fin
+		Calendar calendarFin = Calendar.getInstance();
+		calendarFin.setTime(fechaFin);
+		int diaFin = calendarFin.get(Calendar.DAY_OF_MONTH);
+		int mesFin = calendarFin.get(Calendar.MONTH) + 1; // 0 Enero, 11 Diciembre
+		int anioFin = calendarFin.get(Calendar.YEAR);
+	 
+		int anios = 0;
+		int mesesPorAnio = 0;
+		int diasPorMes = 0;
+		int diasTipoMes = 0;
+		// Calculo de días del mes
+		if (mesInicio == 2) {
+			// Febrero
+			if ((anioFin % 4 == 0) && ((anioFin % 100 != 0) || (anioFin % 400 == 0))) {
+				// Bisiesto
+				diasTipoMes = 29;
+			} else {
+				// No bisiesto
+				diasTipoMes = 28;
+			}
+		} else if (mesInicio <= 7) {
+			// De Enero a Julio los meses pares tienen 30 y los impares 31
+			if (mesInicio % 2 == 0) {
+				diasTipoMes = 30;
+			} else {
+				diasTipoMes = 31;
+			}
+		} else if (mesInicio > 7) {
+			// De Julio a Diciembre los meses pares tienen 31 y los impares 30
+			if (mesInicio % 2 == 0) {
+				diasTipoMes = 31;
+			} else {
+				diasTipoMes = 30;
+			}
+		}
+		// Calculo de diferencia de año, mes y dia
+		//
+		if ((anioInicio > anioFin) || (anioInicio == anioFin && mesInicio > mesFin)
+				|| (anioInicio == anioFin && mesInicio == mesFin && diaInicio > diaFin)) {
+			// La fecha de inicio es posterior a la fecha fin
+			 System.out.println("La fecha de inicio ha de ser anterior a la fecha fin");
+			return -1;
+		} else {
+			if (mesInicio <= mesFin) {
+				anios = anioFin - anioInicio;
+				if (diaInicio <= diaFin) {
+					mesesPorAnio = mesFin - mesInicio;
+					diasPorMes = diaFin - diaInicio;
+				} else {
+					if (mesFin == mesInicio) {
+						anios = anios - 1;
+					}
+					mesesPorAnio = (mesFin - mesInicio - 1 + 12) % 12;
+					diasPorMes = diasTipoMes - (diaInicio - diaFin);
+				}
+			} else {
+				anios = anioFin - anioInicio - 1;
+				System.out.println(anios);
+				if (diaInicio > diaFin) {
+					mesesPorAnio = mesFin - mesInicio - 1 + 12;
+					diasPorMes = diasTipoMes - (diaInicio - diaFin);
+				} else {
+					mesesPorAnio = mesFin - mesInicio + 12;
+					diasPorMes = diaFin - diaInicio;
+				}
+			}
+		}
+		
+		long returnValue = -1;
+	 
+		switch (tipo) {
+			case 0:
+				// Total Años
+				returnValue = anios;
+			    System.out.println("Total años: " + returnValue + " Años.");
+				break;
+	 
+			case 1:
+				// Total Meses
+				returnValue = anios * 12 + mesesPorAnio;
+				System.out.println("Total meses: " + returnValue + " Meses.");
+				break;
+	 
+			case 2:
+				// Total Dias (se calcula a partir de los milisegundos por día)
+				long millsecsPerDay = 86400000; // Milisegundos al dia
+				returnValue = (fechaFin.getTime() - fechaInicio.getTime()) / millsecsPerDay;
+				System.out.println("Total días: " + returnValue + " Días.");
+				break;
+	 
+			case 3:
+				// Meses del año
+				returnValue =mesesPorAnio;
+				System.out.println("Meses del año: " + returnValue);
+				break;
+	 
+			default:
+				break;
+		}	 
+		return returnValue;
+	}
+	
 	
 	
 }
