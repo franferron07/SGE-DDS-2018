f29dc7cf63995ee9d3994dd8976bc599296b2f9c
Francisco <fran.ferron07@gmail.com>
2018-06-17 00:35:42 -0300

    agrego condiciones

--- src/main/java/entities/CondicionRegla.java
+++ src/main/java/entities/CondicionRegla.java
@@ -12,8 +12,41 @@ public class CondicionRegla {
 		this.valorComparable = valorComparable;
 	}
 	
+	//chequeo si se cumple o no la medicion segun el comparador pasado. siempre el valorComparable es el segundo miembro
 	public boolean cumpleMedicion(Float valor) {
-		// TODO Auto-generated method stub
+		
+		if( this.comparador == ">" ){
+			
+			if( valor > this.valorComparable ) return true;
+			return false;
+		}
+		if( this.comparador == "<" ){
+			
+			if( valor < this.valorComparable )return true;
+			return false;
+			
+		}
+		if( this.comparador == "==" ){
+			
+			if( valor == this.valorComparable ) return true;
+			return false;
+		}
+		if( this.comparador == "!=" ){
+			
+			if( valor != this.valorComparable )return true;
+			return false;
+		}
+		if( this.comparador == "<=" ){
+			
+			if( valor <= this.valorComparable )return true;
+			return false;
+		}
+		if( this.comparador == ">=" ){
+			
+			if( valor >= this.valorComparable)return true;
+			return false;
+		}
+		
 		return false;
 	}
 
