42065305782359a71dee700e442c60b0b142072a
lucasdellar <22032525+lucasdellar@users.noreply.github.com>
2018-10-30 07:25:28 -0300

    Más cosas de cliente (falta completar)

--- src/main/java/controllers/AdministradorController.java
+++ src/main/java/controllers/AdministradorController.java
@@ -3,30 +3,52 @@ package controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import repositorios.RepositorioUsuarios;
+import repositorios.RepositorioClientes;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import usuarios.Administrador;
+import usuarios.Cliente;
 import dispositivos.DispositivoDetalle;
 
 public class AdministradorController {
 	
-	public RepositorioUsuarios repo;
+	public RepositorioUsuarios repositorio_usuarios;
+	public RepositorioClientes repositorio_clientes;
 	
 	public AdministradorController(){
-		repo = new RepositorioUsuarios();
+		repositorio_usuarios = new RepositorioUsuarios();
 	}
 	
 	
 	public ModelAndView verHogares(Request request, Response response) {
 		Map<String, Object> model=new HashMap<>();
 		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		
+		LocalDateTime desde = LocalDateTime.parse(request.queryParams("fechaDesde"), formatter);
+		LocalDateTime hasta = LocalDateTime.parse(request.queryParams("fechaHasta"), formatter);
+		
+		Cliente cliente = new Cliente();
+		
 		int id = request.session().attribute("id");
-		Administrador admin = (Administrador) repo.buscarUsuario(id);
+		Administrador admin = (Administrador) repositorio_usuarios.buscarUsuario(id);
 		model.put("admin", admin);
 		return new ModelAndView(model, "administrador.hbs");
 	}
+	
+/*
+	public ModelAndView mostrar(Request request, Response response, boolean permiteEdicion) {
+		Map<String, Object> model=new HashMap<>();
+		int id = Integer.parseInt(request.params("id"));
+		Usuario usuario = this.buscarUsuario(id);
+		model.put("usuario", usuario);
+		model.put("permiteEdicion", permiteEdicion);
+		return new ModelAndView(model, "modalUsuario.hbs");
+}
 
 	public ModelAndView reporte(Request request, Response response) {
 		Map<String, Object> model=new HashMap<>();
@@ -36,6 +58,8 @@ public class AdministradorController {
 		model.put("admin", admin);
 		return new ModelAndView(model, "modalEstado.hbs");
 	}
+	
+*/
 	
 	public ModelAndView crearDispositivo(Request request, Response response) {
 		
