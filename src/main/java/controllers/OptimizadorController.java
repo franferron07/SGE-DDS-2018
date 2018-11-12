2c61fb7c6407606bb3e01745dba432dbd3104050
Daniel <pacdaolmed@est.frba.utn.edu.ar>
2018-11-12 13:06:22 -0300

    modifico  .hbs del optimizador y en Controller , uso layoutCliente.hbs , mostrando horas maxima , falta gson

--- src/main/java/controllers/OptimizadorController.java
+++ src/main/java/controllers/OptimizadorController.java
@@ -15,10 +15,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class OptimizadorController {
-    Cliente cliente_seleccionado=new Cliente();
-    ArrayList<DispositivoUsuario> dispositivos_seleccionados=new ArrayList<DispositivoUsuario>();
-    ArrayList<ResultadoHora> resultados=new ArrayList<ResultadoHora>();
-    ModelHelper modelHelper =new ModelHelper();
+    private    Cliente cliente_seleccionado=new Cliente();
+    private ArrayList<DispositivoUsuario> dispositivos_seleccionados=new ArrayList<DispositivoUsuario>();
+    private ArrayList<ResultadoHora> resultados=new ArrayList<ResultadoHora>();
+    private ModelHelper modelHelper =new ModelHelper();
 
 
     private Cliente buscarClientePorID(int id_usuario){
@@ -27,16 +27,14 @@ public class OptimizadorController {
     }
     public ModelAndView mostrarResultadosOptimizador(Request request, Response res ){
         Map<String, Object> model=new HashMap<>();
-
-        Integer user_id = request.session().attribute("id");
-        //Integer user_id=Integer.parseInt(request.queryParams("id"));
-        this.setearValoresAUsuario();
-        modelHelper.agregar(cliente_seleccionado);
-        cliente_seleccionado = this.buscarClientePorID(cliente_seleccionado.getId());
+//        Integer user_id = request.session().attribute("id");
+        setearUser();
+       // modelHelper.agregar(cliente_seleccionado);
+      //  cliente_seleccionado = this.buscarClientePorID(cliente_seleccionado.getId());
 
 
         Optimizador optimizador = new Optimizador();
-        optimizador.cargarDispositivosEsenciales(cliente_seleccionado);
+        optimizador.cargarDispositivos(this.getUnosDispositivos().get(0),this.getUnosDispositivos().get(1),this.getUnosDispositivos().get(2));
         optimizador.maximizar();
         resultados = optimizador.resultados();
         Boolean se_paso_de_consumo=optimizador.consumioSuMaximaEnergia();
@@ -44,121 +42,138 @@ public class OptimizadorController {
 
         model.put("user_name",cliente_seleccionado.getNombre());
         model.put("user_id",cliente_seleccionado.getId());
-        model.put("resultado_horas",resultados);
+        model.put("resultados",resultados);
         model.put("se_paso_de_consumo",se_paso_de_consumo);
-        return new ModelAndView(model, "optimizador_resultados.hbs");
+        return new ModelAndView(model, "optimizador_resultados_v2.hbs");
     }
-
-    private void setearValoresAUsuario(){
-
-
-        cliente_seleccionado.setNombre("Daniel");
-        DispositivoUsuario lcd,lavaropas,ventilador,heladera;
+    private void setearUser(){
+        cliente_seleccionado.setId(2);
+        cliente_seleccionado.setNombre("UnNombre");
+        cliente_seleccionado.setApellido("UnApellido");
+        cliente_seleccionado.setDispositivos(this.getUnosDispositivos());
+    }
+    private ArrayList<DispositivoUsuario> getUnosDispositivos(){
+       DispositivoUsuario lcd,lavaropas,ventilador,heladera;
         lcd=new DispositivoUsuario() {
 
-            @Override
-            public float consumoPeriodo(LocalDateTime desde, LocalDateTime hasta) {	return 0;}
-            @Override
-            public double getConsumoKwHora() {	return 0.18;
-            }
-            @Override
-            public String getIdentificacion() {	return "lcd";}
-            @Override
-            public double getHsMensualMinimo() {	return 90;}
-            @Override
-            public double getHsMensualMaximo() {	return 370;}
-            @Override
-            public boolean esEsencial() {return true;}
-            @Override
-            public boolean esInteligente() {
-                // TODO Auto-generated method stub
-                return false;
-            }
-            @Override
-            public double horasDeUso(LocalDateTime desde, LocalDateTime hasta) {
-                // TODO Auto-generated method stub
-                return 0;
-            }
-        };
-        lavaropas=new DispositivoUsuario() {
-            @Override
-            public float consumoPeriodo(LocalDateTime desde, LocalDateTime hasta) {	return 0;}
+           @Override
+           public float consumoPeriodo(LocalDateTime desde, LocalDateTime hasta) {	return 0;}
+           @Override
+           public double getConsumoKwHora() {	return 0.18;
+           }
+           @Override
+           public String getIdentificacion() {	return "lcd";}
+           @Override
+           public double getHsMensualMinimo() {	return 90;}
+           @Override
+           public double getHsMensualMaximo() {	return 370;}
+           @Override
+           public boolean esEsencial() {return true;}
+           @Override
+           public boolean esInteligente() {
+               // TODO Auto-generated method stub
+               return false;
+           }
+           @Override
+           public double horasDeUso(LocalDateTime desde, LocalDateTime hasta) {
+               // TODO Auto-generated method stub
+               return 0;
+           }
 
-            @Override
-            public double getConsumoKwHora() {	return 0.875;}
-            @Override
-            public String getIdentificacion() {	return "lavarropas";}
-            @Override
-            public double getHsMensualMinimo() {	return 6;}
-            @Override
-            public double getHsMensualMaximo() {	return 30;}
+       };
+//        lcd.detalle.setEsEsencial(true);
+       lavaropas=new DispositivoUsuario() {
+           @Override
+           public float consumoPeriodo(LocalDateTime desde, LocalDateTime hasta) {	return 0;}
 
-            @Override
-            public boolean esEsencial() {return true;}
+           @Override
+           public double getConsumoKwHora() {	return 0.875;}
+           @Override
+           public String getIdentificacion() {	return "lavarropas";}
+           @Override
+           public double getHsMensualMinimo() {	return 6;}
+           @Override
+           public double getHsMensualMaximo() {	return 30;}
 
-            @Override
-            public boolean esInteligente() {
-                return false;
-            }
+           @Override
+           public boolean esEsencial() {return true;}
 
-            @Override
-            public double horasDeUso(LocalDateTime desde, LocalDateTime hasta) {
-                // TODO Auto-generated method stub
-                return 0;
-            }
-        };
-        ventilador=new DispositivoUsuario() {
-            @Override
-            public float consumoPeriodo(LocalDateTime desde, LocalDateTime hasta) {	return 0;}
-            @Override
-            public double getConsumoKwHora() {	return 0.06;}
-            @Override
-            public String getIdentificacion() {	return "ventilador";}
-            @Override
-            public double getHsMensualMinimo() {	return 120;}
-            @Override
-            public double getHsMensualMaximo() {	return 360;}
-            @Override
-            public boolean esEsencial() {return true;}
-            @Override
-            public boolean esInteligente() {
-                return false;
-            }
-            @Override
-            public double horasDeUso(LocalDateTime desde, LocalDateTime hasta) {
-                // TODO Auto-generated method stub
-                return 0;
-            }
-        };
-        heladera=new DispositivoUsuario() {
-            @Override
-            public float consumoPeriodo(LocalDateTime desde, LocalDateTime hasta) {	return 0;}
-            @Override
-            public double getConsumoKwHora() {	return 0;}
-            @Override
-            public String getIdentificacion() {	return "heladera";}
-            @Override
-            public double getHsMensualMinimo() {	return 0;}
-            @Override
-            public double getHsMensualMaximo() {	return 0;}
-            @Override
-            public boolean esEsencial() {return true;}
-            @Override
-            public boolean esInteligente() {
-                return false;
-            }
-            @Override
-            public double horasDeUso(LocalDateTime desde, LocalDateTime hasta) {
-                // TODO Auto-generated method stub
-                return 0;
-            }
-        };
+           @Override
+           public boolean esInteligente() {
+               return false;
+           }
 
-        cliente_seleccionado.agregarDispositivo(lcd);
-        cliente_seleccionado.agregarDispositivo(lavaropas);
-        cliente_seleccionado.agregarDispositivo(ventilador);
-        cliente_seleccionado.agregarDispositivo(heladera);
+           @Override
+           public double horasDeUso(LocalDateTime desde, LocalDateTime hasta) {
+               // TODO Auto-generated method stub
+               return 0;
+           }
+       };
+  //      lavaropas.detalle.setEsEsencial(true);
+       ventilador=new DispositivoUsuario() {
+           @Override
+           public float consumoPeriodo(LocalDateTime desde, LocalDateTime hasta) {	return 0;}
+           @Override
+           public double getConsumoKwHora() {	return 0.06;}
+           @Override
+           public String getIdentificacion() {	return "ventilador";}
+           @Override
+           public double getHsMensualMinimo() {	return 120;}
+           @Override
+           public double getHsMensualMaximo() {	return 360;}
+           @Override
+           public boolean esEsencial() {return true;}
+           @Override
+           public boolean esInteligente() {
+               return false;
+           }
+           @Override
+           public double horasDeUso(LocalDateTime desde, LocalDateTime hasta) {
+               // TODO Auto-generated method stub
+               return 0;
+           }
+       };
+    //    ventilador.detalle.setEsEsencial(true);
+       heladera=new DispositivoUsuario() {
+           @Override
+           public float consumoPeriodo(LocalDateTime desde, LocalDateTime hasta) {	return 0;}
+           @Override
+           public double getConsumoKwHora() {	return 0;}
+           @Override
+           public String getIdentificacion() {	return "heladera";}
+           @Override
+           public double getHsMensualMinimo() {	return 0;}
+           @Override
+           public double getHsMensualMaximo() {	return 0;}
+           @Override
+           public boolean esEsencial() {return true;}
+           @Override
+           public boolean esInteligente() {
+               return false;
+           }
+           @Override
+           public double horasDeUso(LocalDateTime desde, LocalDateTime hasta) {
+               // TODO Auto-generated method stub
+               return 0;
+           }
+       };
+/*       Optimizador simplex = new Optimizador();
+       simplex.setConsumoMaximoDeEnergia(450000);
+       simplex.cargarDispositivos(lcd,lavaropas,ventilador);
+       simplex.maximizar();
+       resultados=simplex.getHorasDeCadaDispositivo();*/
+       this.dispositivos_seleccionados.add(lcd);
+       this.dispositivos_seleccionados.add(lavaropas);
+       this.dispositivos_seleccionados.add(ventilador);
+       return this.dispositivos_seleccionados;
+   }
 
-        cliente_seleccionado.id=1;
-    }
+
+//    @Test
+//    public void horasMaximas() {
+//        Assert.assertEquals(resultados.get(0).getHorasQuePuedeConsumir(),370.0,0.001);
+//        Assert.assertEquals(resultados.get(1).getHorasQuePuedeConsumir(),30.0,0.001);
+//        Assert.assertEquals(resultados.get(2).getHorasQuePuedeConsumir(),360.0,0.001);
+//        Assert.assertEquals(resultados.get(0).getDispositivo(),"lcd");
+//    }
 }
